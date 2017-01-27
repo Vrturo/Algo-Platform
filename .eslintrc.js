cdd0e4799a455aaa4dc65411d2cab55a39361b2a
Arturo Perez <VrturoPerez@gmail.com>
2017-01-26 16:31:26 -0800

    allow exceptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,17 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
+    ],
+    "rules": {
+        // disable
+        "no-console": "off"
+    },
+    valid: [
+      {
+          code: "Vue"
+      },
+      {
+          code: "$"
+      }
     ]
 };
